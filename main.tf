7fd183b2bce797773db6f5510c0722f91b45ca17
shetterjacob <99507851+shetterjacob@users.noreply.github.com>
2024-06-13 17:08:55 -0400

    First functional environment
    This is the first functional GCP environment built in Terrform. This code contains the following:
    
    One firewall rule allowing ssh to a tag: jitterbit-access
    Two VMs with one attached disk each, 8vCPUs, 16GB RAM and Ubuntu OS
    Two directly attached persistent storage disks
    A network called Jitterbit-vpc
    A subnetwork at 10.16.128.0/24 called jitterbit-subnet.

--- main.tf
+++ main.tf
@@ -19,86 +19,154 @@ resource "google_compute_subnetwork" "placeholder" {
     name          = "${var.name}-subnet"   // give the subnet name
     ip_cidr_range = var.ip_cidr_range       //give the cidr_range
     region        = var.region     // give the region
-    network       = google_compute_network.jitterbit.id  //specify vpc
+    network       = google_compute_network.placeholder.self_link  //specify vpc
 }
 
-resource "google_compute_network_firewall_policy_rule" "jitterbit-rdp" {
-  action                  = "allow"
-  description             = "This is a rule to allow RDP from known IP addresses"
-  direction               = "INGRESS"
-  disabled                = false
-  enable_logging          = true
-  firewall_policy         = google_compute_network_firewall_policy.jitterbit-rdp.self_link
-  priority                = 1000
-  rule_name               = "jitterbit-rdp"
+resource "google_compute_firewall" "jitterbit-access" {
+  project       = var.project_id
+  name          = "jitterbit-access"
+  network       = google_compute_network.placeholder.self_link
+  description   = "Creates firewall rule targeting tagged instances"
+  source_ranges = ["71.120.182.247/32"]
+  target_tags   = ["jitterbit-access"]
 
-  match {
-    src_ip_ranges = ["71.120.182.247/32"]
-    src_region_codes = ["US"]
-    src_threat_intelligences = ["iplist-known-malicious-ips"]
-    }
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+}
 
-    layer4_configs {
-      ip_protocol = "tcp"
-      ports       = "3389"
-    }
-        target_secure_tags {
-          name    = "jitterbit-access"
-        }
+resource "google_compute_disk" "dev-disk" {
+  name  = "dev-disk"
+  type  = "pd-balanced"
+  zone  = "us-central1-a"
+  size  = 100
+  labels = {
+    environment = "dev"
+  }
+}
 
-    src_address_groups = [google_network_security_address_group.basic_global_networksecurity_address_group.id]
+resource "google_compute_disk" "prod-disk" {
+  name  = "prod-disk"
+  type  = "pd-balanced"
+  zone  = "us-central1-a"
+  size  = 100
+  labels = {
+    environment = "prod"
+  }
 }
 
 
-resource "google_compute_instance" "production" {
-  name            = "${var.name}-prod"
-  machine_type    = var.vm_type
-  zone            = var.zone
-  tags            = ("jitterbit-prod","jitterbit-access")
+resource "google_compute_instance" "dev" {
+  machine_type        = "n2-custom-8-16384"
+  name                = "${var.name}-dev"
+  zone                = var.zone
+  tags                = ["jitterbit-access"]
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  attached_disk {
+    source      = google_compute_disk.dev-disk.self_link
+    mode        = "READ_WRITE"
+  }
 
   boot_disk {
+    auto_delete = true
+    device_name = "boot_disk"
+
     initialize_params {
-      image         = "Windows Server 2022 Datacenter"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240607"
+      size  = 10
+      type  = "pd-balanced"
     }
-  }
 
-  compute_disk {
-    name    = "${var.name}-dev"
-    type    = "pd-balanced"
-    size    = 100
-  }
-  network_interface {
-    network         = google_compute_network.placeholder.self_link
-    subnetwork      = google_compute_subnetwork.placeholder.self_link
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
-
-resource "google_compute_instance" "development" {
-  name            = "${var.name}-dev"
-  machine_type    = var.vm_type
-  zone            = var.zone
-  tags            = ("jitterbit-dev"), ("jitterbit-access")
-
-  boot_disk {
-    initialize_params {
-      image = "Windows Server 2022 Datacenter"
-    }
-  }
-  compute_disk {
-    name    = "${var.name}-dev"
-    type    = "pd-balanced"
-    size    = 100
+    mode = "READ_WRITE"
   }
 
   network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
     network         = google_compute_network.placeholder.self_link
     subnetwork      = google_compute_subnetwork.placeholder.self_link
-    access_config {
-      // Ephemeral IP
-    }
   }
 
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "1079502158005-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+}
+
+resource "google_compute_instance" "prod" {
+  machine_type        = "n2-custom-8-16384"
+  name                = "${var.name}-prod"
+  zone                = var.zone
+  tags                = ["jitterbit-access"]
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  attached_disk {
+    source      = google_compute_disk.prod-disk.self_link
+    mode        = "READ_WRITE"
+  }
+
+  boot_disk {
+    auto_delete = true
+    device_name = "boot_disk"
+
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240607"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    network         = google_compute_network.placeholder.self_link
+    subnetwork      = google_compute_subnetwork.placeholder.self_link
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "1079502158005-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
 }
