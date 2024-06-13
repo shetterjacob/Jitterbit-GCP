ed3a94a8edbbed714ba7fffaf8ca33ff519e7d66
shetterjacob <99507851+shetterjacob@users.noreply.github.com>
2024-06-13 13:00:52 -0400

    Variables for environment
    This is the initial environments file that main.tf will connect with.

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  type           = string
+  description    = "Project ID for intended working environment"
+  default        = "vibrant-fabric-286015"
+}
+
+variable "region" {
+  type           = string
+  description    = "Region for this infrastructure"
+  default        = "us-central1"
+}
+
+variable "zone" {
+  type           = string
+  description    = "Zone for this infrastructure"
+  default        = "us-central1-a"
+}
+
+variable "name" {
+  type           = string
+  description    = "Name for this infrastructure"
+  default        = "Jitterbit"
+}
+
+variable "ip_cidr_range" {
+  type           =list(string)
+  description    ="List of The range of internal addresses that are owned by this subnetwork."
+  default        =["10.16.128.0/24"]
+}
+
+variable "vm_type" {
+  type           = string
+  description    ="Type of VM to be used in Compute creation."
+  default        ="n2-highcpu-8"
+}
